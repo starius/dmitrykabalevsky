0ef006ac6c8fff27c0359726bd7c6aaf232a47d7
boris <bnagaev@gmail.com>
2011-03-28 18:39:50 +0400

    upload.sh: fixes

--- upload.sh
+++ upload.sh
@@ -1,20 +1,23 @@
 COOKIES='/tmp/dm.cookies'
-CURL="curl -b $COOKIES -c $COOKIES --insecure -H 'Expect:' "
+CURL="curl -b $COOKIES -c $COOKIES --insecure -H 'Expect:' > /dev/null 2> /dev/null "
 
 read -s -p "Web interface password: " web_password
-zip 1.zip * --exclude config.php
+FILES=`echo *.php *.html *.txt | sed 's/config.php//'`
+echo $FILES
+zip 1.zip $FILES
 
 touch $COOKIES
 chmod 600 $COOKIES
 
 
-$CURL http://dmitrykabalevsky.ru/.filemanager/?login=w_dmitrykabale-ru_1c7bb3f6\&password=$web_password
-$CURL -F 'fichier=@1.zip' -F Directory=. -F action=upload -F sort=NomASC http://dmitrykabalevsky.ru/.filemanager/index.php 
-for i in *.php *.txt *.html; do
-    $CURL -d "fichier=$i&action=deleterimerFichier&Directory=.&rep=&sort=NomASC" http://dmitrykabalevsky.ru/.filemanager/index.php
+eval "$CURL http://dmitrykabalevsky.ru/.filemanager/?login=w_dmitrykabale-ru_1c7bb3f6\&password=$web_password"
+eval "$CURL -F fichier=@1.zip -F Directory=. -F action=upload -F sort=NomASC http://dmitrykabalevsky.ru/.filemanager/index.php"
+
+for i in $FILES; do
+    eval "$CURL -d \"fichier=$i&action=deleterimerFichier&Directory=.&rep=&sort=NomASC\" http://dmitrykabalevsky.ru/.filemanager/index.php"
 done
-$CURL 'http://dmitrykabalevsky.ru/.filemanager/unpack.php?fichier=1.zip&Directory=.&sort=NomASC'
-$CURL -d 'fichier=1.zip&action=deleterimerFichier&Directory=.&rep=&sort=NomASC' http://dmitrykabalevsky.ru/.filemanager/index.php
+eval "$CURL 'http://dmitrykabalevsky.ru/.filemanager/unpack.php?fichier=1.zip&Directory=.&sort=NomASC'"
+eval "$CURL -d 'fichier=1.zip&action=deleterimerFichier&Directory=.&rep=&sort=NomASC' http://dmitrykabalevsky.ru/.filemanager/index.php"
 
 rm $COOKIES
 rm 1.zip
