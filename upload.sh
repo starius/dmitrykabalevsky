8660274057461cafca723802ac9dcf339b6456a8
boris <bnagaev@gmail.com>
2011-03-28 18:01:50 +0400

    upload.sh: upload files through web interface

---
+++ upload.sh
@@ -0,0 +1,24 @@
+COOKIES='/tmp/dm.cookies'
+#WGET=wget --no-check-certificate --keep-session-cookies \
+#    --load-cookies=$COOKIES --save-cookies=$COOKIES
+CURL="curl -b $COOKIES -c $COOKIES --insecure -H 'Expect:' "
+
+read -s -p "Web interface password: " web_password
+zip 1.zip * --exclude config.php
+
+touch $COOKIES
+chmod 600 $COOKIES
+
+
+$CURL http://dmitrykabalevsky.ru/.filemanager/?login=w_dmitrykabale-ru_1c7bb3f6\&password=$web_password
+$CURL -F 'fichier=@1.zip' -F Directory=. -F action=upload -F sort=NomASC http://dmitrykabalevsky.ru/.filemanager/index.php 
+for i in *.php *.txt *.html; do
+    $CURL -d "fichier=$i&action=deleterimerFichier&Directory=.&rep=&sort=NomASC" http://dmitrykabalevsky.ru/.filemanager/index.php
+done
+$CURL 'http://dmitrykabalevsky.ru/.filemanager/unpack.php?fichier=1.zip&Directory=.&sort=NomASC'
+$CURL -d 'fichier=1.zip&action=deleterimerFichier&Directory=.&rep=&sort=NomASC' http://dmitrykabalevsky.ru/.filemanager/index.php
+
+rm $COOKIES
+rm 1.zip
+
+
